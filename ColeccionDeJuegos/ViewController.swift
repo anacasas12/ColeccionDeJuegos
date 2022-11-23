76dc9ce3d89ae1264a40316b579d7fad2c617bc5
Ana Cecilia Casas Carrion <ana.casas@tecsup.edu.pe>
2022-11-23 01:27:15 -0800

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Ana Casas on 11/23/22.
+//  Copyright © 2022 Ana Casas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
